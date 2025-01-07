d5319525e102ffefde0bfdfe315c0917ee364aa1
Supari K <manoj@mith.tech>
2025-01-07 09:50:48 +0000

    latest changes

---
+++ justsigns_custom/justsigns_custom/doctype/sheet_link/sheet_link.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, MithTech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sheet Link", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ justsigns_custom/justsigns_custom/doctype/sheet_link/test_sheet_link.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, MithTech and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSheetLink(FrappeTestCase):
+	pass
